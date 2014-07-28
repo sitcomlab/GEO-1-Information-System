3a2e51c336de17e449bc1186fdc6d7f2bd67a22d
Markus <mkonkol@arcor.de>
2014-07-28 15:05:27 +0200

    delete all

--- php/logData.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbconn = pg_connect("host=giv-konkol.uni-muenster.de port=5432 dbname=GEO1 user=postgres password=navigeo1")
-    or die('Verbindungsaufbau fehlgeschlagen: ' . pg_last_error());
-	
-	$issue = $_POST['Actions'];
-
-	$query=pg_query($dbconn,"Insert into \"LoggedData\" values (DEFAULT,'".$issue."');");
-	echo $issue;
-?>
